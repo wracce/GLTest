ab2eec538e91614ac9b2f7ef289eeeb0895f2d8d
Wracce <wracce@gmail.com>
2022-03-06 16:05:19 +0400

    Exercise 1

--- src/main.cpp
+++ src/main.cpp
@@ -46,8 +46,8 @@ int WINAPI WinMain(HINSTANCE hInstance,
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
-		256,
-		256,
+		500,
+		500,
 		NULL,
 		NULL,
 		hInstance,
@@ -79,7 +79,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
 		{
 			/* OpenGL animation code goes here */
 
-			glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+			glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 			glClear(GL_COLOR_BUFFER_BIT);
 
 			glPushMatrix();
@@ -88,8 +88,12 @@ int WINAPI WinMain(HINSTANCE hInstance,
 			glBegin(GL_TRIANGLES);
 
 			glColor3f(1.0f, 0.0f, 0.0f);   glVertex2f(0.0f, 1.0f);
-			glColor3f(0.0f, 1.0f, 0.0f);   glVertex2f(0.87f, -0.5f);
-			glColor3f(0.0f, 0.0f, 1.0f);   glVertex2f(-0.87f, -0.5f);
+			glColor3f(0.0f, 1.0f, 0.0f);   glVertex2f(0.0f, 0.0f);
+			glColor3f(0.0f, 0.0f, 1.0f);   glVertex2f(1.0f, 0.0f);
+
+			glColor3f(1.0f, 0.0f, 0.0f);   glVertex2f(0.0f, 1.0f);
+			glColor3f(0.0f, 1.0f, 0.0f);   glVertex2f(1.0f, 1.0f);
+			glColor3f(0.0f, 0.0f, 1.0f);   glVertex2f(1.0f, 0.0f);
 
 			glEnd();
 

57a25c316dd976ba6a06de7f138543732aa123f1
Wracce <wracce@gmail.com>
2022-03-26 01:51:21 +0400

    Ex 14

--- src/main.cpp
+++ src/main.cpp
@@ -5,57 +5,199 @@
 #include <math.h>
 #include <time.h>
 #include <iostream>
-#define _USE_MATH_DEFINES
+#include <mmsystem.h>
+
+#pragma comment(lib, "winmm.lib")
 #pragma comment(lib, "opengl32.lib")
 
 LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
 void EnableOpenGL(HWND hwnd, HDC*, HGLRC*);
 void DisableOpenGL(HWND, HDC, HGLRC);
 
-float k;
-int width, height;
+HWND hwnd;
+
+float kube[] = {0,0,0, 0,1,0 , 1,1,0 , 1,0,0 , 0,0,1 , 0,1,1 , 1,1,1 , 1,0,1 };
+GLuint kubeInd[] = { 0,1,2, 2,3,0, 4,5,6, 6,7,4, 3,2,5, 6,7,3, 0,1,5, 5,4,0, 1,2,6, 6,5,1, 0,3,7, 7,4,0 };
+
+BOOL showMask = false;
 
 typedef struct {
-	char name[20];
-	float vert[8];
-	BOOL hover;
-} TButton ;
+	float r, g, b;
+} TColor;
 
-TButton btn[] = {
-	"start", {0,0,100,0,100,30,0,30}, FALSE,
-	"stop", {0,40,100,40,100,70,0,70}, FALSE ,
-	"quit", {0,80,100,80,100,110,0,110}, FALSE
-};
+typedef struct {
+	TColor clr;
+} TCell;
 
-int btnCnt = sizeof(btn) / sizeof(btn[0]);
+#define pW 40
+#define pH 40
+TCell map[pW][pH];
 
-void TButton_Show(TButton btn)
+void Map_Init() {
+	for (int i = 0; i < pW; i++)
+	{
+		for (int j = 0; j < pH; j++)
+		{
+			float dc = (rand() % 20) * 0.01;
+			map[i][j].clr.r = 0.31 + dc;
+			map[i][j].clr.g = 0.5 + dc;
+			map[i][j].clr.b = 0.13 + dc;
+
+		}
+	}
+}
+
+#define enemyCnt 40
+typedef struct {
+	float x, y, z;
+	bool active;
+} TEnemy;
+TEnemy enemy[enemyCnt];
+
+void Enemy_Init() {
+	for (int i = 0; i < enemyCnt; i++)
+	{
+		enemy[i].active = TRUE;
+		enemy[i].x = rand() % pW;
+		enemy[i].y = rand() % pH;
+		enemy[i].z = rand() % 5;
+	}
+}
+
+void Enemy_Show()
 {
 	glEnableClientState(GL_VERTEX_ARRAY);
-	if (btn.hover) glColor3f(1, 0, 0);
-	else glColor3f(0, 1, 0);
-	glVertexPointer(2, GL_FLOAT, 0, btn.vert);
-	glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+	glVertexPointer(3, GL_FLOAT, 0, kube);
+	for (int i = 0; i < enemyCnt; i++)
+	{
+		if (!enemy[i].active) continue;
+		glPushMatrix();
+		glTranslatef(enemy[i].x, enemy[i].y, enemy[i].z);
+		if (showMask)
+			glColor3ub(255-i, 0, 0);
+		else glColor3ub(244,60,43);
+		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, kubeInd);
+		glPopMatrix();
+
+	}
+	glDisableClientState(GL_VERTEX_ARRAY);
+	glPopMatrix();
+}
+
+struct {
+	float x, y, z;
+	float Xrot, Zrot;
+} camera = {0,0,1.7, 70, -40};
+
+void Camera_Apply() {
+	glRotatef(-camera.Xrot, 1, 0, 0);
+	glRotatef(-camera.Zrot, 0, 0, 1);
+	glTranslatef(-camera.x, -camera.y, -camera.z);
+}
+
+void Camera_Rotation(float xAngle, float zAngle) {
+	camera.Zrot += zAngle;
+	if (camera.Zrot < 0) camera.Zrot += 360;
+	if (camera.Zrot > 360) camera.Zrot -= 360;
+	camera.Xrot += xAngle;
+	if (camera.Xrot < 0) camera.Xrot = 0;
+	if (camera.Xrot > 180) camera.Xrot = 180;
+}
+
+void Player_Move() {
+	if (GetForegroundWindow() != hwnd) return;
+
+	float ugol = -camera.Zrot / 180 * M_PI;
+	float speed = 0;
+
+	if (GetKeyState('W') < 0) speed = 0.1;
+	if (GetKeyState('S') < 0) speed = -0.1;
+	if (GetKeyState('A') < 0) { speed = 0.1; ugol -= M_PI * 0.5; }
+	if (GetKeyState('D') < 0) { speed = 0.1; ugol += M_PI * 0.5; }
+	if (speed != 0)
+	{
+		camera.x += sin(ugol) * speed;
+		camera.y += cos(ugol) * speed;
+	}
+
+	POINT cur;
+	static POINT base = { 400,300 };
+	GetCursorPos(&cur);
+	Camera_Rotation((base.y-cur.y)/5.0,(base.x-cur.x)/5.0);
+	SetCursorPos(base.x, base.y);
+}
+
+
+void WndResize(int x, int y);
+
+void Game_Move() {
+	Player_Move();
+}
+
+void Game_Init() {
+	glEnable(GL_DEPTH_TEST);
+	Map_Init();
+	Enemy_Init();
+
+	RECT rct;
+	GetClientRect(hwnd, &rct);
+	WndResize(rct.right, rct.bottom);
+}
+
+void Game_Show() {
+	if(showMask)
+		glClearColor(0, 0, 0, 0);
+	else
+		glClearColor(0.6, 0.8, 1, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glPushMatrix();
+	Camera_Apply();
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 0, kube);
+	for (int i = 0; i < pW; i++)
+	{
+		for (int j = 0; j < pH; j++)
+		{
+			glPushMatrix();
+			glTranslatef(i, j, 0);
+			if (showMask)
+				glColor3f(0,0,0);
+			else
+				glColor3f(map[i][j].clr.r, map[i][j].clr.g, map[i][j].clr.b);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, kubeInd);
+			glPopMatrix();
+
+		}
+	}
 	glDisableClientState(GL_VERTEX_ARRAY);
 
-}
+	Enemy_Show();
 
-BOOL PointInButton(int x, int y, TButton btn)
-{
-	return((x > btn.vert[0]) && (x < btn.vert[4])
-		&& (y > btn.vert[1]) && (y < btn.vert[5]));
-}
-
-void ShowMenu()
-{
-	glPushMatrix();
-		glLoadIdentity();
-		glOrtho(0,width,height,0,-1,1);
-		for (int i = 0; i < btnCnt; i++)
-		{
-			TButton_Show(btn[i]);
-		}
 	glPopMatrix();
+}
+
+void Player_Shoot() {
+	showMask = TRUE;
+	Game_Show();
+	showMask = FALSE;
+
+	RECT rct;
+	GLubyte clr[3];
+	GetClientRect(hwnd, &rct); 
+	glReadPixels(rct.right / 2.0, rct.bottom / 2.0, 1, 1,GL_RGB,GL_UNSIGNED_BYTE,clr);
+	if (clr[0] > 0)
+		enemy[255 - clr[0]].active = FALSE;
+
+	PlaySound("shoot.wav", NULL, SND_ASYNC);
+}
+
+void WndResize(int x, int y) {
+	glViewport(0, 0, x, y);
+	float k = x / (float)y;
+	float sz = 0.1;
+	glLoadIdentity();
+	glFrustum(-k*sz, k * sz,-sz,sz,sz*2,100);
 }
 
 int WINAPI WinMain(HINSTANCE hInstance,
@@ -64,7 +206,6 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	int nCmdShow)
 {
 	WNDCLASSEX wcex;
-	HWND hwnd;
 	HDC hDC;
 	HGLRC hRC;
 	MSG msg;
@@ -96,8 +237,8 @@ int WINAPI WinMain(HINSTANCE hInstance,
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
-		500,
-		500,
+		800,
+		800,
 		NULL,
 		NULL,
 		hInstance,
@@ -107,6 +248,8 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 	/* enable OpenGL for the window */
 	EnableOpenGL(hwnd, &hDC, &hRC);
+
+	Game_Init();
 
 	/* program main loop */
 	while (!bQuit)
@@ -128,14 +271,9 @@ int WINAPI WinMain(HINSTANCE hInstance,
 		else
 		{
 			/* OpenGL animation code goes here */
+			Game_Move();
+			Game_Show();
 
-			glClearColor(0, 0, 0, 0);
-			glClear(GL_COLOR_BUFFER_BIT);
-			
-			glPushMatrix();
-			glPopMatrix();
-
-			ShowMenu();
 
 			SwapBuffers(hDC);
 
@@ -158,32 +296,31 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CLOSE:
 		PostQuitMessage(0);
-	break;
-	case WM_LBUTTONDOWN:
-		for (int i = 0; i < btnCnt; i++)
-		{
-			if (PointInButton(LOWORD(lParam), HIWORD(lParam), btn[i]))
-			{
-				std::cout << btn[i].name << std::endl;
-				if (strcmp(btn[i].name, "quit") == 0)
-					PostQuitMessage(0);
-			}
-		}
 		break;
-	case WM_MOUSEMOVE:
-		for (int i = 0; i < btnCnt; i++)
-		{
-			btn[i].hover = PointInButton(LOWORD(lParam), HIWORD(lParam), btn[i]);
-		}
-		break;
+
 	case WM_SIZE:
-		width = LOWORD(lParam);
-		height = HIWORD(lParam);
-		glViewport(0, 0, width, height);
-		glLoadIdentity();
-		k = width / (float)height;
-		glOrtho(-1*k, k, -1, 1, -1, 1);
+		WndResize(LOWORD(lParam), HIWORD(lParam));
 		break;
+
+	case WM_SETCURSOR:
+		ShowCursor(FALSE);
+		break;
+	case WM_LBUTTONDOWN:
+		Player_Shoot();
+	case WM_DESTROY:
+		return 0;
+
+	case WM_KEYDOWN:
+	{
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+			PostQuitMessage(0);
+			break;
+		}
+	}
+	break;
+
 	default:
 		return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
